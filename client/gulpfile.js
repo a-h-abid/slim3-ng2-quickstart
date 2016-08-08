97eb94d940ebba69804fc373ed602fb74c9befe0
Ahmedul Haque Abid <abdmaster2003@hotmail.com>
2016-08-08 12:51:34 +0600

    Update gulpfile.js

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -12,6 +12,20 @@ gulp.task('app.build', function () {
         .pipe(gulp.dest('../public/ng2/app'));
 });
 
+gulp.task('vendor.copy', function() {
+    var vendorsToCopy = [
+        'node_modules/@angular/**/*',
+        'node_modules/angular2-in-memory-web-api/**/*',
+        'node_modules/rxjs/**/*',
+        'node_modules/ng2-i18next/**/*',
+        'node_modules/i18next/**/*',
+        'node_modules/i18next-xhr-backend/**/*',
+        'node_modules/i18next-sprintf-postprocessor/**/*',
+    ];
+    
+    return gulp.src(vendorsToCopy, {base:"./node_modules"})
+                .pipe(gulp.dest('../public/ng2/vendor')); 
+});
 
 
 gulp.task('default', ['app.build']);
